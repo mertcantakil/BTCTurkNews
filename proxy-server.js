361a3470800806c6561e455bcb03bae9bfaca24a
Mertcan Takıl <mertcantakil@gmail.com>
2024-05-12 22:55:53 +0300

    firebase config set

--- proxy-server.js
+++ proxy-server.js
@@ -1,25 +1,29 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const API_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey=bf36a75abb0d4e639e920c2b3178403e'; // API anahtarınızı buraya ekleyin
 
-// Firebase Hosting'e yönlendirilecek isteklerin ana endpoint'i
-const API_ENDPOINT = 'https://btcturkpro-c3eb7.web.app/';
-
-// Proxy middleware'i oluşturun
-const apiProxy = createProxyMiddleware({
-  target: API_ENDPOINT,
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '', // Firebase Hosting'e gelen isteklerde '/api' prefix'i kaldırılıyor
-  },
+// CORS hatasını önlemek için gerekli başlıkları ayarlayın
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-// '/api' endpoint'i için proxy kullan
-app.use('/api', apiProxy);
+// /api endpoint'ine gelen istekleri API'ye yönlendirin
+app.get('/api', async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-// Express sunucusunu başlat
-const PORT = process.env.PORT || 3000;
+// Sunucuyu belirtilen portta başlatın
 app.listen(PORT, () => {
-  console.log(`Proxy server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
